fdf5c6154873661f04badf5418def812f421fa52
ChristianAarup1 <15209@student.eadania.dk>
2021-11-09 13:08:36 +0100

    MonoGame
    Added mongame files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tower_Defense
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
