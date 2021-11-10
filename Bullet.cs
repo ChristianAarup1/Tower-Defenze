4664467df676e2bbebd95a95fcb130ff23aa26a4
ChristianAarup1 <15209@student.eadania.dk>
2021-11-10 10:01:15 +0100

    Projectile
    Added an abstract projectile class

---
+++ Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defense
+{
+    class Bullet : Projectile
+    {
+        public void OnHit()
+        {
+
+        }
+    }
+}
+
+    
+
