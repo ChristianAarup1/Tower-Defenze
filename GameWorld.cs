cea1587e9efd8170a4dbf7f3c8751115fdac2a0b
MichelleTheDragon <mh@maoeh.dk>
2021-11-10 11:22:59 +0100

    beginning

--- GameWorld.cs
+++ GameWorld.cs
@@ -9,7 +9,20 @@ namespace Tower_Defense
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        private List<Tower> myTowers = new List<Tower>();
+        private List<Enemy> myEnemies = new List<Enemy>();
+        private int[,] enemyMovePattern = new int[,] { { 2, 0 }, { 2, 3 }, { 4, 3 } };
+        private int[,] enemyWaves = new int[,] { { 20, 0, 0 }, {10, 5, 2 } };
+        private int currentWave = 0;
+        private int enemiesOfTypeSpawned = 0;
+        private int currentTypeSpawn = 0;
+
+        private int gold = 0;
+        private float timeToWave = 30.0f;
+        private float timeBetweenEnemies = 1.0f;
+
+
+        public GameWorld()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -37,16 +50,90 @@ namespace Tower_Defense
 
             // TODO: Add your update logic here
 
+            foreach (Tower myTower in myTowers)
+            {
+                myTower.Update(gameTime);
+            }
+            foreach (Enemy myEnemy in myEnemies)
+            {
+                myEnemy.Update(gameTime);
+            }
+
+            if(timeToWave <= 0)
+            {
+                SpawnEnemyWave(gameTime);
+            } else
+            {
+                timeToWave -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            foreach (Tower myTower in myTowers)
+            {
+                myTower.Draw(_spriteBatch);
+            }
+
+            foreach (Enemy myEnemy in myEnemies)
+            {
+                myEnemy.Draw(_spriteBatch);
+            }
+
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        public void GoldDrop(int gold)
+        {
+            this.gold = gold;
+        }
+
+        public void SpawnEnemyWave(GameTime gameTime)
+        {
+            if(timeBetweenEnemies <= 0)
+            {
+                if (enemiesOfTypeSpawned >= enemyWaves[currentWave, currentTypeSpawn])
+                {
+                    enemiesOfTypeSpawned = 0;
+                    currentTypeSpawn++;
+                    if (currentTypeSpawn > 3)
+                    {
+                        currentTypeSpawn = 0;
+                        timeToWave = 30.0f;
+                    }
+                }
+
+                if (currentTypeSpawn != 0) {
+
+                    switch (currentTypeSpawn)
+                    {
+                        case 1:
+                            myEnemies.Add(new Normal(enemyMovePattern));
+                            break;
+                        case 2:
+                            myEnemies.Add(new Fast(enemyMovePattern));
+                            break;
+                        case 3:
+                            myEnemies.Add(new Strong(enemyMovePattern));
+                            break;
+                    }
+                    enemiesOfTypeSpawned++;
+                    timeBetweenEnemies = 1.0f;
+                }
+            } else
+            {
+                timeBetweenEnemies -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
         }
     }
 }
